cd36ea368579ba480466b8a1929912f825ef8251
Pawel Sobkowiak <pawel.sobkowiak@gmail.com>
2011-04-16 03:31:08 +0200

    variable names cleanup

--- pathfinding/finders.py
+++ pathfinding/finders.py
@@ -28,33 +28,33 @@ class NoPathFound(Exception):
 #    return abs(x1 - x2) + abs(y1 - y2) + abs(z1 - z2)
 
 
-def find_path(departure, destination, max_fields_checked=1000000):
+def find_path(src, dst, max_fields_checked=1000000):
     """ Implementation of A* algorithm """
     global logger
-    if departure == destination:
+    if src == dst:
         return []
     success = None
     start_time = time()
 
+    dx, dy, dz = dst.xyz
+    x, y, z = src.xyz
+    heuristic = (abs(x - dx) + abs(y - dy) + abs(z - dz))
     ### costs = { field: (g, h, parent), ... }
-    costs = {}
-    dx, dy, dz = destination.xyz
-    x, y, z = departure.xyz
-    first_heuristic = (abs(x - dx) + abs(y - dy) + abs(z - dz))
-    costs[departure] = (0, first_heuristic, None)
-    ### open_ordered = [(path_g[field] + path_h[field], field), ...]
-    opened_ordered = [(first_heuristic, departure)]
-    opened = set([departure])
+    costs = {src: (0, heuristic, None)}
+    ### open_ordered = [(g + h, field), ...]
+    opened_ordered = [(heuristic, src)]
+    opened = set([src])
     closed = set()
+
     while opened_ordered:
-        current_field = opened_ordered.pop(0)[1]
+        field = opened_ordered.pop(0)[1]
         # optimalization - it is better to check if field is already
         # in closed list, than to remove touple from opened_ordered list
-        if current_field in closed:
+        if field in closed:
             continue
 
         # check if destination has been achieved
-        if current_field == destination:
+        if field == dst:
             success = True
             break
         elif len(closed) > max_fields_checked:
@@ -62,56 +62,56 @@ def find_path(departure, destination, max_fields_checked=1000000):
             break
 
         # move field to closed list
-        opened.remove(current_field)
-        closed.add(current_field)
+        opened.remove(field)
+        closed.add(field)
 
         # check every neighbouring fields
-        current_g = costs[current_field][0]
-        for connection in current_field.connections:
+        field_g = costs[field][0]
+        for connection in field.connections:
             cost = connection.cost
             neighbour = connection.destination
             if cost == NOT_PASSABLE or neighbour in closed:
                 continue
-            cost += current_g
+            cost += field_g
             if neighbour in opened:
-                ng, nh, nparent = costs[neighbour]
-                if ng > cost:
+                old_g, h, parent = costs[neighbour]
+                if cost < old_g:
                     # update field cost
-                    costs[neighbour] = (cost, nh, current_field)
-                    bisect.insort(opened_ordered, (cost + nh, neighbour))
+                    costs[neighbour] = (cost, h, field)
+                    bisect.insort(opened_ordered, (cost + h, neighbour))
             else:
                 # add field to opened list
                 x, y, z = neighbour.xyz
                 heuristic = (abs(x - dx) + abs(y - dy) + abs(z - dz))
-                costs[neighbour] = (cost, heuristic, current_field)
+                costs[neighbour] = (cost, heuristic, field)
                 bisect.insort(opened_ordered,
                               (cost + heuristic, neighbour))
                 opened.add(neighbour)
 
     if success is None:
         logger.error("No path found - opened list empty - find_path(%s, %s)",
-                     departure, destination)
+                     src, dst)
         raise NoPathFound("opened list empty - find_path(%s, %s)"
-                          % (departure, destination))
+                          % (src, dst))
 
     # backtracing path
     path = []
-    parent = costs[current_field][2]
+    parent = costs[field][2]
     while parent:
-        path.append(current_field)
-        current_field = parent
-        parent = costs[current_field][2]
+        path.append(field)
+        field = parent
+        parent = costs[field][2]
 
     # calculating stats
-    total_cost = costs[destination][0] if success else -1
+    total_cost = costs[dst][0] if success else -1
     calculation_time = time() - start_time
     logger.debug("astar %.3f %s->%s lenght=%i closed=%i total_cost=%i heur=%i",
-            calculation_time, departure.xyz, destination.xyz, len(path),
-            len(closed), total_cost, first_heuristic)
+            calculation_time, src.xyz, dst.xyz, len(path),
+            len(closed), total_cost, costs[src][1])
     return path
 
 
-def find_nearest_targets(departure, target_getter,
+def find_nearest_targets(src, target_getter,
                          count=1, max_distance=100000):
     """
     Uses Dijkstra algorithm to find fields that has any target
@@ -121,49 +121,49 @@ def find_nearest_targets(departure, target_getter,
     destinations = []
     start_time = time()
 
-    path_cost = {departure: 0}
-    path_parent = {departure: None}
-    opened_ordered = [(0, departure)]
-    opened = set([departure])
+    path_cost = {src: 0}
+    path_parent = {src: None}
+    opened_ordered = [(0, src)]
+    opened = set([src])
     closed = set()
     while opened_ordered:
-        current_cost, current_field = opened_ordered.pop(0)
+        cost, field = opened_ordered.pop(0)
         # optimalization - it is better to check if field is already
         # in closed list, than to remove touple from opened_ordered list
-        if current_field in closed:
+        if field in closed:
             continue
 
         # check if we achieved destination
-        if path_cost[current_field] > max_distance:
+        if path_cost[field] > max_distance:
             break
-        targets = target_getter(current_field)
+        targets = target_getter(field)
         if targets:
-            destinations.append((current_field, targets))
+            destinations.append((field, targets))
             if len(destinations) >= count:
                 break
 
         # move to closed list
-        opened.remove(current_field)
-        closed.add(current_field)
+        opened.remove(field)
+        closed.add(field)
 
         # check every neighbouring field
-        for connection in current_field.connections:
+        for connection in field.connections:
             neighbour_cost = connection.cost
             neighbour = connection.destination
             if neighbour_cost == NOT_PASSABLE or neighbour in closed:
                 continue
-            neighbour_cost += current_cost
+            neighbour_cost += cost
             if neighbour in opened:
                 if path_cost[neighbour] > neighbour_cost:
                     # update field cost
                     path_cost[neighbour] = neighbour_cost
-                    path_parent[neighbour] = current_field
+                    path_parent[neighbour] = field
                     bisect.insort(opened_ordered,
                                   (neighbour_cost, neighbour))
             else:
                 # add to opened list
                 path_cost[neighbour] = neighbour_cost
-                path_parent[neighbour] = current_field
+                path_parent[neighbour] = field
                 bisect.insort(opened_ordered, (neighbour_cost, neighbour))
                 opened.add(neighbour)
 
@@ -183,5 +183,5 @@ def find_nearest_targets(departure, target_getter,
     # calculating stats
     calculation_time = time() - start_time
     logger.debug("dijkstra %.3f from %s paths_found=%i closed_list=%i",
-            calculation_time, departure.xyz, len(paths), len(closed))
+            calculation_time, src.xyz, len(paths), len(closed))
     return paths
