b0c075d2efbabc2edb9e667a13a7854559a81236
Pawel Sobkowiak <pawel.sobkowiak@gmail.com>
2011-05-24 13:07:33 +0200

    changed export graph to json function

---
+++ pathfinding/_performance.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+u'''Performance framework for pathfinding.
+
+mallib: common library for mal projects
+@author: Paweł Sobkowiak
+@contact: pawel.sobkowiak@gmail.com
+Copyright © 2011 Paweł Sobkowiak
+
+'''
+
--- pathfinding/tools.py
+++ pathfinding/tools.py
@@ -11,9 +11,10 @@ Copyright © 2011 Paweł Sobkowiak
 
 from collections import deque
 import json
+import random
 
 
-def export_to_json(entry_node, fp=None):
+def export_to_json(entry_node, fp=None, sample_size=10):
     """Exports graph to JSON.
 
     Walks the graph starting from entry_node.
@@ -22,17 +23,22 @@ def export_to_json(entry_node, fp=None):
     """
     queue = deque([entry_node])
     opened = set([entry_node])
-    data = []
+    graph = []
     while queue:
         node = queue.pop()
-        row = list(node.xyz) + [[]]
+        row = [node.xyz.x, node.xyz.y, node.xyz.z, []]
         for connection in node.connections:
             dst = connection.destination
             if dst not in opened:
                 opened.add(dst)
                 queue.append(dst)
-            row[3].append(list(dst.xyz) + [int(connection.cost)])
-        data.append(row)
+            row[3].append([dst.xyz.x, dst.xyz.y, dst.xyz.z,
+                           int(connection.cost)])
+        graph.append(row)
+    opened = list(opened)
+    sample = random.sample(opened, min(len(opened), sample_size))
+    sample = [list(node.xyz) for node in sample]
+    data = {'graph': graph, 'sample': sample}
 
     if fp is not None:
         json.dump(data, fp)
