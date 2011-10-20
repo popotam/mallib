fdc6e537d1fb7400d580e883783ed9a45a97df05
Paweł Sobkowiak <pawel.sobkowiak@gmail.com>
2011-10-20 23:38:47 +0200

    Added support for IPython>=0.11

--- shell.py
+++ shell.py
@@ -9,14 +9,22 @@ Copyright © 2011 Paweł Sobkowiak
 
 '''
 
+from distutils.version import LooseVersion
 
-def interactive_shell(globals_dict):
+
+def interactive_shell(namespace):
     try:
-        globals().update(globals_dict)
-        from IPython.Shell import IPShellEmbed
-        ipshell = IPShellEmbed([],
-                               "Starting interactive shell",
-                               "Leaving interactive shell")
-        ipshell(header='')
-    except:
+        import IPython
+        if LooseVersion(IPython.__version__) >= LooseVersion('0.11'):
+            IPython.embed(user_ns=namespace,
+                          banner1="Starting interactive shell",
+                          exit_msg="Leaving interactive shell")
+        else:
+            from IPython.Shell import IPShellEmbed
+            ipshell = IPShellEmbed(
+                    banner="Starting interactive shell",
+                    exit_msg="Leaving interactive shell",
+                    user_ns=namespace)
+            ipshell()
+    except ImportError:
         print "Unable to enter interactive mode - could not import ipython"
